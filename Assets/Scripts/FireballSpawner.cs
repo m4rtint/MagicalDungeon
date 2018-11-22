c0ccd857662e6d08a88c9110fe7e77067b5b38e1
kazenourufu <31673776+kazenourufu@users.noreply.github.com>
2018-11-22 12:42:50 -0800

    Object Pooling
    I have write a simple object pooler with some tests.

---
+++ Assets/Scripts/FireballSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballSpawner : MonoBehaviour {
+
+    ObjectPooler objectPooler;
+
+    private void Start()
+    {
+        objectPooler = ObjectPooler.Instance;
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse Pressed");
+            GameObject obj = objectPooler.SpawnFromPool("fireball", transform.position, Quaternion.identity);
+            IPooledObject pooledObj =  obj.GetComponent<FireballMovement>();
+            if (pooledObj != null)
+            {
+                pooledObj.OnObjectSpawn();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour {
+
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    #region Singleton
+    public static ObjectPooler Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    #endregion Singleton
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>>  poolDictionary;
+
+
+	// Use this for initialization
+	void Start () {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach(Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+
+            poolDictionary.Add(pool.tag,  objectPool);
+        }
+	}
+
+    public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
+    {
+        if (!poolDictionary.ContainsKey(tag))
+        {
+            Debug.LogWarning(tag + " does not exist");
+            return null;
+        }
+       
+        GameObject newObj =   poolDictionary[tag].Dequeue();
+        newObj.SetActive(true);
+        newObj.transform.position = position;
+        newObj.transform.rotation = rotation;
+
+        poolDictionary[tag].Enqueue(newObj);
+
+        return newObj;
+    }
+
+
+}
