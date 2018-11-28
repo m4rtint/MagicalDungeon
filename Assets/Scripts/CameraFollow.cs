1a5ca2e9e015912a1416911e3f443cbc99b51527
Thai Ngoc Nguyen <thaingoc@alumni.ubc.ca>
2018-11-27 18:47:36 -0800

    Added CameraFollow, a script allowing the camera to follow the player

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    [SerializeField] float smoothTime = 0.05F;
+    private Vector3 velocity = Vector3.zero;
+
+    void Update()
+    {
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 0, -10));
+        transform.position = Vector3.SmoothDamp(transform.position,
+                                                targetPosition,
+                                                ref velocity,
+                                                smoothTime);
+
+    }
+}
