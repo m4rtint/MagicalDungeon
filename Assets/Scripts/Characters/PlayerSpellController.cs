f01e57b922b5273e1819d12b3f931aab4c6020b4
Martin <m4rtin.t@gmail.com>
2019-01-10 19:43:33 -0800

    One Spell Thrown

--- Assets/Scripts/Characters/PlayerSpellController.cs
+++ Assets/Scripts/Characters/PlayerSpellController.cs
@@ -14,8 +14,25 @@ public class PlayerSpellController : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        activateSpellConeIfNeeded();
+        //activateSpellConeIfNeeded();
         updateSpellConeRotation();
+        activateFireball();
+    }
+
+    void activateFireball()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 dir = Input.mousePosition - Utilities.worldToScreenObjectPosition(gameObject);
+            float angle = Utilities.getAngleDegBetween(dir.y, dir.x);
+            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            GameObject fireball = ObjectPooler.Instance.SpawnFromPool("Fireball", transform.position, rotation);
+            fireball.GetComponent<Fireball>().OnObjectSpawn();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            coneSpellHolder.SetActive(false);
+        }
     }
 
     #region Spells
