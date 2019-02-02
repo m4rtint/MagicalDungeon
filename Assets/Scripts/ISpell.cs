bb60ebdf8cf90d42b370274c2ae7c83b5d1ccaeb
Martin <m4rtin.t@gmail.com>
2019-02-01 16:30:29 -0800

    Refactor and skill AOE in progress

---
+++ Assets/Scripts/ISpell.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class ISpell : MonoBehaviour
+{
+
+    [SerializeField] float velocity;
+    [SerializeField] protected float spellMovementTimeToLive;
+    [SerializeField] protected float damage;
+
+    protected float angle;
+    protected float currentTimeToLive; //currentDuration
+    protected bool isMoving = true;
+
+    private string[] listOfObstacleTags = { Tags.ENEMY, Tags.SOLID_OBSTACLE };
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        if (isMoving)
+        {
+            transform.Translate(VectorFromAngle(angle) * velocity);
+            currentTimeToLive += Time.fixedDeltaTime;
+            if (currentTimeToLive > spellMovementTimeToLive)
+            {
+                onMovementTimeToLiveStopped();
+            }
+        }
+    }
+
+    protected virtual void OnTriggerEnter2D(Collider2D col)
+    {
+        checkIfSpellHitObject(col.tag);
+    }
+
+    void checkIfSpellHitObject(string tag)
+    {
+        for (int i = 0; i < listOfObstacleTags.Length; i++)
+        {
+            if (tag == listOfObstacleTags[i])
+            {
+                onSpellHitObject();
+            }
+        }
+    }
+
+    protected abstract void onSpellHitObject();
+
+    protected abstract void onMovementTimeToLiveStopped();
+
+
+    #region Tools
+    private Vector2 VectorFromAngle(float theta)
+    {
+        return new Vector2(Mathf.Cos(theta), Mathf.Sin(theta));
+    }
+    #endregion
+
+}
--- Assets/Scripts/Spells/Fireball.cs
+++ Assets/Scripts/Spells/Fireball.cs
@@ -2,48 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fireball : MonoBehaviour, IPooledObject
+public class Fireball : ISpell, IPooledObject
 {
 
-    [SerializeField] float velocity;
-    [SerializeField] float secondsDuration;
-    [SerializeField] float damage;
-    private float angle;
-    private float currentDuration;
-    private string[] listOfObstacleTags = { Tags.ENEMY, Tags.SOLID_OBSTACLE };
+    //[SerializeField] float velocity;
+    //[SerializeField] float secondsDuration;
+    //[SerializeField] float damage;
+    //private float angle;
+    //private float currentDuration;
+    //private string[] listOfObstacleTags = { Tags.ENEMY, Tags.SOLID_OBSTACLE };
 
-    private bool isMoving = true;
+    //private bool isMoving = true;
 
     //Animation
-    readonly string ANIMATION_EXPLOSION = "Explode";
+    const string ANIMATION_EXPLOSION = "Explode";
+    const float EXPLOSION_ANIMATION_TIME = 0.4f;
 
     // Update is called once per frame
-    void Update()
+    //void Update()
+    //{
+    //    if (isMoving) { 
+    //        transform.Translate(VectorFromAngle(angle) * velocity);
+    //        currentDuration += Time.fixedDeltaTime;
+    //        if (currentDuration > secondsDuration)
+    //        {
+    //            gameObject.SetActive(false);
+    //        }
+    //    }
+    //}
+
+    protected override void onMovementTimeToLiveStopped()
     {
-        if (isMoving) { 
-            transform.Translate(VectorFromAngle(angle) * velocity);
-            currentDuration += Time.fixedDeltaTime;
-            if (currentDuration > secondsDuration)
-            {
-                gameObject.SetActive(false);
-            }
-        }
+        gameObject.SetActive(false);
     }
 
-    Vector2 VectorFromAngle(float theta)
-    {
-        return new Vector2(Mathf.Cos(theta), Mathf.Sin(theta)); 
-    }
+
+    //Vector2 VectorFromAngle(float theta)
+    //{
+    //    return new Vector2(Mathf.Cos(theta), Mathf.Sin(theta)); 
+    //}
 
     public void OnObjectSpawn()
     {
         Vector3 dir = transform.rotation.eulerAngles;
         angle = Utilities.getAngleDegBetween(dir.y, dir.x) + 90;
-        currentDuration = 0;
+        base.currentTimeToLive = 0;
         isMoving = true;
     }
 
-    void OnTriggerEnter2D(Collider2D col)
+    protected override void OnTriggerEnter2D(Collider2D col)
     {
         GameObject hitTarget = col.gameObject;
         if (hitTarget.tag == Tags.ENEMY)
@@ -51,22 +58,29 @@ public class Fireball : MonoBehaviour, IPooledObject
             hitTarget.GetComponent<EnemyController>().decrementHealth(damage);
         }
 
-        deactivateObjectIfNeeded(hitTarget.tag); 
+        base.OnTriggerEnter2D(col);
     }
 
-    void deactivateObjectIfNeeded(string tag)
+    //void deactivateObjectIfNeeded(string tag)
+    //{
+    //    for(int i = 0; i < listOfObstacleTags.Length; i++)
+    //    {
+    //        if (tag == listOfObstacleTags[i])
+    //        {
+    //            isMoving = false;
+    //            GetComponent<Animator>().SetTrigger(ANIMATION_EXPLOSION);
+    //        }
+    //    }
+    //}
+
+    protected override void onSpellHitObject()
     {
-        for(int i = 0; i < listOfObstacleTags.Length; i++)
-        {
-            if (tag == listOfObstacleTags[i])
-            {
-                isMoving = false;
-                GetComponent<Animator>().SetTrigger(ANIMATION_EXPLOSION);
-            }
-        }
+        isMoving = false;
+        GetComponent<Animator>().SetTrigger(ANIMATION_EXPLOSION);
+        Invoke("onFinishExplosionAnimation", EXPLOSION_ANIMATION_TIME);
     }
 
-    public void onFinishExplosionAnimation()
+    void onFinishExplosionAnimation()
     {
         gameObject.SetActive(false);
     }
