edf453a6b9e3964777d3c83b26a59c2700046d34
Thai Ngoc Nguyen <thaingoc@alumni.ubc.ca>
2019-02-06 19:14:17 -0800

    Boss health bar now appear when player enter vicinity of boss

---
+++ Assets/BossRoom.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossRoom : MonoBehaviour {
+
+    [SerializeField]
+    GameObject boss;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        GameObject entered = col.gameObject;
+        if (entered.tag == Tags.PLAYER && !boss.GetComponent<BossController>().awakened)
+        {
+            boss.GetComponent<BossController>().awakened = true;
+            boss.GetComponent<BossController>().healthBar.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Characters/BossController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : ICharacter {
+
+    public bool awakened;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        healthBar.SetActive(false);
+    }
+
+    protected override  void Update()
+    {
+        if (awakened)
+        {
+
+        }
+    }
+
+
+
+
+}
