60ebd6a97ebbc71ff8784b6b19461c96496c5788
Thai Ngoc Nguyen <thaingoc@alumni.ubc.ca>
2019-03-08 00:28:03 -0800

    finished adding start button that initiates the game

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour {
+
+    public bool isStart;
+    public bool isQuit;
+
+    void OnMouseClick()
+    {
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    }
+}
---
+++ Assets/Scripts/MouseHover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MouseHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler {
+
+    Text myText;
+
+    void Start()
+    {
+        myText = GetComponent<Text>();
+    }
+
+    public void OnPointerEnter (PointerEventData eventData)
+    {
+        myText.color = Color.white;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        myText.color = Color.black;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    }
+}
