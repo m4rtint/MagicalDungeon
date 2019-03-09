c29ea5962f4916f2232bc91a518121332b0ad5f2
Martin Tsang <m4rtin.t@gmail.com>
2019-03-08 21:14:22 -0800

    Sound and fadeing main menu and bug fixes

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    [SerializeField]
+    GameObject fade;
+
+    [SerializeField]
+    AudioSource backgroundMusic;
+
+    public static MenuManager instance = null;
+    const float fadeTime = 2f;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void fadeOut()
+    {
+        Hashtable var = new Hashtable();
+        var.Add("easetype", "easeInQuart");
+        var.Add("scale", new Vector3(100f, 100f));
+        var.Add("time", fadeTime);
+        var.Add("oncompletetarget", gameObject);
+        var.Add("oncomplete", "loadNextScene");
+
+        iTween.ScaleTo(fade, var);
+
+
+
+        Hashtable sound = new Hashtable();
+        sound.Add("time", fadeTime);
+        sound.Add("audiosource", backgroundMusic);
+        sound.Add("volume", 0);
+        iTween.AudioTo(gameObject, sound);
+    }
+
+
+    void loadNextScene()
+    {
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    }
+
+}
--- Assets/Scripts/MouseHover.cs
+++ Assets/Scripts/MouseHover.cs
@@ -2,30 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MouseHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler {
+public class MouseHover : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler {
+
+
+    [SerializeField]
+    AudioClip hover;
+    [SerializeField]
+    AudioClip click;
 
     Text myText;
+    AudioSource audioSource;
+    bool gameStart = false;
 
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         myText = GetComponent<Text>();
     }
 
     public void OnPointerEnter (PointerEventData eventData)
     {
-        myText.color = Color.white;
+        if (!gameStart) { 
+            playSound(hover);
+        }
     }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        myText.color = Color.black;
-    }
-
+    
     public void OnPointerDown(PointerEventData eventData)
     {
-        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+        playSound(click);
+        MenuManager.instance.fadeOut();
+        gameStart = true;
+    }
+
+
+
+    void playSound(AudioClip clip)
+    {
+        audioSource.clip = clip;
+        audioSource.Play();
     }
 }
